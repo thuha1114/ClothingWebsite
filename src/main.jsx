67470cba08e9fc9bd039f156febf14e3d230a8f8
Thaoduyendoto <94709525+Thaoduyendoto@users.noreply.github.com>
2024-03-29 17:00:42 +0700

    admin

--- src/main.jsx
+++ src/main.jsx
@@ -20,8 +20,7 @@ import { Confirm } from './pages/Confirm.jsx';
 import { Login } from './pages/Login.jsx';
 import { ForgetPass } from './pages/ForgetPass.jsx';
 import {SignUp} from './pages/SignUp.jsx';
-
-
+import {Admin} from './pages/Admin.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -66,7 +65,13 @@ const router = createBrowserRouter([
   {
     path: "/forget-pass",
     element: <ForgetPass/>,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
   }
+  
+  
 ]);
 
 
